ff0ba0607752842027f8a8026c206b4759d413cb
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2024-03-10 16:06:22 -0400

    view all users

--- accounts/views/admin_views.py
+++ accounts/views/admin_views.py
@@ -2,8 +2,16 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
+from ..serializer.user_serializer import UserSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def users_view(_):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
