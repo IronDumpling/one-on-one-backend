695941ed680372756e4016f03614bddb9000140a
xingrun jiao <xingrun.jiao@mail.utoronto.ca>
2024-03-09 23:21:17 -0500

    members get works

--- meetings/views/member_views.py
+++ meetings/views/member_views.py
@@ -2,45 +2,57 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from ..models import meeting,member
-from ..serializer import meeting_serializer,member_serializer
 
+from ..models.meeting import Meeting
+from ..models.member import Member
+
+from ..serializer.member_serializer import MemberSerializer
 
 
 
 @api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def member_list_view(request, meeting_id):
 
-    try:
-       Meeting = meeting.objects.get(pk=meeting_id)
-    except meeting.DoesNotExist:
+    members = Member.objects.filter(meeting=meeting_id)
+    if members == None:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
-        serializer =meeting_serializer(Meeting)
+        serializer = MemberSerializer(members, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
 
-        serializer = meeting_serializer(data=request.data)
+        serializer = MemberSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(Meeting=Meeting)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def member_view(request, meeting_id, member_id):
-    try:
-        Meeting = meeting.objects.get(pk=meeting_id)
-    except meeting.DoesNotExist:
+    
+    member = Member.objects.get(pk=member_id,meeting = meeting_id)
+    if member== None:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    try:
-        Member = member.objects.get(pk=member_id, meeting=meeting)
-    except member.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = MemberSerializer(member)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = MemberSerializer(member, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        member.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
