8cfaba8b4f8f69c822d507d5b84594e6ed5c083b
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2024-03-10 16:15:26 -0400

    profile get and put

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,10 @@ from .views import contact_views, admin_views
 app_name = 'accounts'
 
 urlpatterns = [
-    # path('', ),
     # path('<int:user_id>/', ),
     path('', admin_views.users_view, name='users'),
     path('register/', admin_views.register_view, name='register'),
+    path('profile/', admin_views.profile_view, name='profile'),
     path('contacts/', contact_views.contact_list_view, name="contact_list"),
     path('contacts/<int:contact_id>/', contact_views.contact_view, name="contact"),
 ]
--- accounts/views/admin_views.py
+++ accounts/views/admin_views.py
@@ -2,9 +2,10 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from ..serializer.user_serializer import UserSerializer
+from django.contrib.auth.hashers import make_password
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
@@ -26,3 +27,26 @@ def register_view(request):
         return Response({"message": "Register success."}, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def profile_view(request):
+    match request.method:
+        case 'GET':
+            serializer = UserSerializer(request.user)
+            return Response(serializer.data)
+        case 'PUT':
+            user = request.user
+            data = request.data
+
+            if 'email' in data and data['email'] is not None:
+                user.email = data['email']
+
+            if 'password' in data and data['password'] is not None:
+                user.password = make_password(data['password'])
+
+            user.save()
+
+            # Return the updated user data
+            serializer = UserSerializer(user)
+            return Response({'message: Update Profile success'}, status=status.HTTP_200_OK)
