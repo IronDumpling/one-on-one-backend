26c0961aac79847bb8348d92b7f11f6153097fbe
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2024-03-09 22:54:43 -0500

    create contact model

--- accounts/models/contact.py
+++ accounts/models/contact.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Contact(models.Model):
-    # id = models.AutoField(primary_key=True, unique=True)
-    # user_1 = models.ForeignKey(User)
-    # user_2 = models.ForeignKey(User)
-    pass
+    id = models.AutoField(primary_key=True, unique=True)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
