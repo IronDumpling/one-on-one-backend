fbbe4fa9551ec2c6a6f4dbbd3248fe85b95dfecb
Xuhui Chen <xuhui.chen@mail.utoronto.ca>
2024-03-10 00:11:13 -0500

    refine post contact logic

--- accounts/serializer/contact_serializer.py
+++ accounts/serializer/contact_serializer.py
@@ -1,17 +1,8 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from ..models.contact import Contact
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
-
 class ContactSerializer(serializers.ModelSerializer):
-    user1_detail = UserSerializer(source='user1', read_only=True)
-    user2_detail = UserSerializer(source='user2', read_only=True)
 
     class Meta:
         model = Contact
-        fields = ['id', 'user1', 'user2', 'user1_detail', 'user2_detail']
-
+        fields = ['id', 'user1', 'user2']
--- accounts/views/contact_views.py
+++ accounts/views/contact_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from ..serializer.contact_serializer import ContactSerializer
 from django.db import models
 from ..models.contact import Contact
+from django.contrib.auth.models import User
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -12,13 +13,27 @@ def contact_list_view(request):
     match request.method:
         case 'GET':
             user = request.user
-            print('current_user: ' + user.__str__())
             contacts = Contact.objects.filter(models.Q(user1=user) | models.Q(user2=user))
             serializer = ContactSerializer(contacts, many=True)
             return Response(serializer.data)
         case 'POST':
             data = request.data.copy()
             data.update({'user1': request.user.id})
+
+            user1 = request.user
+            user2_id = data.get('user2')
+
+            if str(user1.id) == str(user2_id):
+                return Response({"error": "Cannot add yourself as contact."}, status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                user2 = User.objects.get(pk=user2_id)
+            except User.DoesNotExist:
+                return Response({"error": "User2 does not exist."}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Check if the contact already exists
+            if Contact.objects.filter(models.Q(user1=user1, user2=user2) | models.Q(user1=user2, user2=user1)).exists():
+                return Response({"error": "Contact already exists."}, status=status.HTTP_400_BAD_REQUEST)
 
             serializer = ContactSerializer(data=data)
             if serializer.is_valid():
