63b2ec42b69be4d25a4a77cc2ccf769b11ee5fd6
Chuyue Zhang <irondumpling233@outlook.com>
2024-03-10 21:31:00 -0400

    node basics done

--- meetings/serializer/node_serializer.py
+++ meetings/serializer/node_serializer.py
@@ -1,11 +1,32 @@
 from rest_framework import serializers
-from ..models import node
+from ..models.node import RemindNode, JoinNode, SubmitNode, PollNode, StateNode
 
 
-class NodeSerializer(serializers.ModelSerializer):
-
-    # meeting = serializers.ReadOnlyField(source='meeting.name')
-
+class RemindNodeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = node.Node
+        model = RemindNode
+        fields = '__all__'
+
+
+class JoinNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JoinNode
+        fields = '__all__'
+
+
+class SubmitNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubmitNode
+        fields = '__all__'
+
+
+class PollNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollNode
+        fields = '__all__'
+
+
+class StateNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StateNode
         fields = '__all__'
