2928e44f6ae5270a2812fd9c25c83d6d886936dd
Torres Yu <torrestyu2017@gmail.com>
2024-03-10 23:03:05 -0400

    update custom permission class

--- meetings/permissions.py
+++ meetings/permissions.py
@@ -4,27 +4,26 @@ from .models.calendar import Calendar
 from .models.event import Event
 from .models.meeting import Meeting
 
-class IsMember(permissions.BasePermission):
-    def has_permission(self, request, view, **kwargs):
-        if request.user.is_authenticated:
-            meeting_id = kwargs.get('meeting_id')
-            return Member.objects.filter(user=request.user, meeting_id=meeting_id).exists()
-        return False
-
-
 # class IsMember(permissions.BasePermission):
-#     """
-#     Custom permission to only allow members of a meeting, calendar, or event to access it.
-#     """
-#     def has_object_permission(self, request, view, obj):
-#         # Check for membership in Meeting directly
-#         if isinstance(obj, Meeting):
-#             return Member.objects.filter(meeting=meeting, user=request.user).exists()
-        
-#         # Check for membership through Calendar or Event's Meeting
-#         elif isinstance(obj, Calendar) or isinstance(obj, Event):
-#             meeting = obj.meeting
-#             return Member.objects.filter(meeting=meeting, user=request.user).exists()
-        
-#         # Default deny if the object doesn't match
+#     def has_permission(self, request, view, **kwargs):
+#         if request.user.is_authenticated:
+#             meeting_id = kwargs.get('meeting_id')
+#             return Member.objects.filter(meeting_id=meeting_id).exists()
 #         return False
+
+class IsMember(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        
+        if isinstance(obj, Meeting):
+            print('1')
+            return obj.member_set.filter(user=request.user).exists()
+        
+        elif isinstance(obj, Calendar):
+            print('2')
+            return obj.meeting.member_set.filter(user=request.user).exists()
+        
+        elif isinstance(obj, Event):
+            print('3')
+            return obj.calendar.meeting.member_set.filter(user=request.user).exists()
+        
+        return False
